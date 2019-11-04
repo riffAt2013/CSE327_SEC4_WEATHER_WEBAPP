f945aaceb8075259eb7dbeefcc05ac7380b3479b
ShoileeH <56479488+shoilee9597@users.noreply.github.com>
2019-11-04 20:24:32 +0600

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-
-function turnOverLayOn(){
-  document.getElementById('overlaywrapper').style.display = 'flex';
-}
-function turnOverLayOff(){
-  document.getElementById('overlaywrapper').style.display = 'none';
-}
