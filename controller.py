6765b848520b0047ba238e7ed61e8cc996a7489a
riffAt2013 <rifat.masud@northsouth.edu>
2019-11-27 13:17:59 +0600

    working 1

--- controller.py
+++ controller.py
@@ -1,7 +1,22 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "HELLONEARTH"
+bootstrap = Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('', validators=[InputRequired(), Length(min=8, max=80)])
+    
+class RegisterForm(FlaskForm):
+    email = StringField('', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 # base / index
 @app.route('/')
@@ -9,17 +24,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup', methods = ['GET', 'POST'])
+@app.route('/signup.html')
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+       return '<h1>' + form.username.data + ' ' + form.password.data + ' '+ form.email.data +'</h1>'
+
+    return render_template("signup.html", form = form)
+
+@app.route('/login', methods = ['GET', 'POST'])
 @app.route('/loginform.html')
 def login():
-    return render_template('loginform.html')
+    form = LoginForm()
 
-@app.route('/search.html')
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('loginform.html', form = form)
+
+@app.route('/citysearch.html')
 def search():
     return render_template('citysearch.html')
-
-# @app.route('/forecast.html')
-# def forecast():
-#     return render_template('forecastpage.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
